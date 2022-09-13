7ef7bd1698b8e6fbc1ca52a125ec875f68329954
Žan Pukmajster <45610204+pukmajster@users.noreply.github.com>
2022-09-13 16:59:57 +0200

    Prep some files

---
+++ backend/cli.py
@@ -0,0 +1,19 @@
+
+"""Modman CLI"""
+import argparse
+
+
+def parse_arguments():
+    """Parse commandline arguments"""
+    parser = argparse.ArgumentParser(usage="%(prog)s", description="Manage installed L4D2 mods",
+                                     epilog="Repo : https://github.com/pukmajster/l4d2-modman")
+    parser.add_argument('-b', '--build', help='generate mod cache')
+    return parser.parse_args()
+
+
+def main():
+    """Start here"""
+    args = parse_arguments()
+
+    if args.build:
+        print("building cache")
