ffc7bb1aa2dc804cf3e2b945535dee4bf3da0eb9
Žan Pukmajster <45610204+pukmajster@users.noreply.github.com>
2022-09-22 15:27:41 +0200

    Remove all traces of python code

--- backend/constants.py
+++
@@ -1,8 +0,0 @@
-"""Various static file paths modman deals with"""
-import os
-
-MODMAN_CACHE_DIR = os.path.expanduser('~/.modman/cache.json')
-
-GAME_DIR = ""
-GAME_WORKSHOP_ADDONS_DIR = GAME_DIR + "/addons/workshop/"
-GAME_LOCAL_ADDONS_DIR = GAME_DIR + "/addons/"
